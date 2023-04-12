f9c5e1da432dc7c28f9e7bf35add7becb04b70bb
Adeola <68297427+adex-hub@users.noreply.github.com>
2023-04-12 06:29:13 +0100

    nearly done

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,12 +65,100 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+/*  ARRAY INTRODUCTION
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4)); // Just like in strings the end parameter (which is 'e' in this case) is not printed to console.
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+
+// They are both useful for creating shallow copies of an array.
+console.log(arr.slice());
+console.log([...arr]);
+
+
+// SPLICE - It mutates the original array & Most times its actual value doesn't interest us as it is commonly used in JS to delete elements of an array.
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2); //arr.splice(index, no. of elements to be deleted starting from that index);
+console.log(arr);
+
+// REVERSE - mutates the original array.
+arr =  ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT - doesn't mutate the original array.
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+console.log(letters.join(' - '));
+*/
+
+/*
+// THE NEW ES 2022 AT METHOD. ///////////////////
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0)); // This is a feature of ES2022 - It works just the same as the line of code above.
+
+// But there are some paticularites
+console.log(arr[arr.length-1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+// The new At method is perfect for METHOD CHAINING and counting an array from behind or finding the last element of an array as seen in the example above. - The At method also works on Strings.
+console.log('Freddy'.at(0));
+*/
+
+/*
+// Introduction to the forEach Method.
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements.entries()) {
+  if(movement > 0){
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
+  }
+}
+
+console.log('==== FOR EACH ====')
+movements.forEach(function(mov, i, arr){
+  if(mov > 0){
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`)
+  }
+})
+
+//  One fundamental difference between the forEach loop and the for of loop is that no matter what you do you cannot break out of a forEach loop (continue and break don't work in JavaScript).
+*/
+
+// Maps
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+currencies.forEach(function(value, key, map){
+  console.log( `${key} : ${value}`);
+})
 
-/////////////////////////////////////////////////
+// Sets
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+currenciesUnique.forEach(function(value, _, map){
+  console.log( `${value} : ${value}`);
+})
+
+// The underscore means a throwaway variable (variable that is completely unnecessary) in JavaScript.
+
