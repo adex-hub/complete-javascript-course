d9b21a668c557855357f673a6874f589a91d3b02
Adeola <68297427+adex-hub@users.noreply.github.com>
2023-04-23 06:43:09 +0100

    Just starting with OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,50 @@
 'use strict';
+
+const Person = function(firstName, birthYear){
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never do this
+    // this.calcAge = function(){
+    //     console.log(2037 - this.birthYear);
+    // }
+}
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas)
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically returns {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+
+// Prototypes
+Person.prototype.calcAge = function(){
+    console.log(2037 - this.birthYear);
+}
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// properties can also be set on prototype not just methods.
+
+Person.prototype.species = 'Homo sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species')); // This is false because species is only a property of the prototype not the jonas object.
+// Person.prototype is actually not a prototype of person but a prototype of all the objects created through the person function.
